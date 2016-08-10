6bcf0aed6e1ba75518b2b29989edadc6ecad1ddc
Emin Mastizada <emin@mastizada.com>
2016-08-10 18:17:53 +0300

    django 1.10 compability fixed in urls.py

--- src/urls.py
+++ src/urls.py
@@ -2,10 +2,10 @@
 
 from __future__ import unicode_literals
 
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from .views import sms_status_callback_view
 
-urlpatterns = patterns("",
+urlpatterns = [
     url(r"^callback/sent/(?P<pk>\d+)/$", sms_status_callback_view, name="sms_status_callback"),
-)
+]
