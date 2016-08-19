5de8bd6e4565abc57b2f6a032895cf5434f0c4b4
Emin Mastizada <emin@mastizada.com>
2016-08-19 15:40:57 +0300

    for some reason twilio dependency doesn't return rfc2822 formatted date.

--- src/utils.py
+++ src/utils.py
@@ -79,9 +79,7 @@ def send_sms(request, to_number, body, callback_urlname="sms_status_callback"):
         if sent.price:
             message.price = Decimal(force_text(sent.price))
             message.price_unit = sent.price_unit
-        rfc2822_time = strptime(str(sent.date_created), '%a, %d %b %Y %H:%M:%S +0000')
-        sent_at = strftime('%Y-%m-%d %H:%M:%S', rfc2822_time)
-        message.sent_at = sent_at
+        message.sent_at = sent.date_created
         message.save(update_fields=[
             "sms_sid", "account_sid", "status", "to_parsed",
             "price", "price_unit", "sent_at"
